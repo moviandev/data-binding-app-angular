2e95e7bbe91627f983558c70e059599c3ed2c2d3
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-02 19:00:18 -0300

    chore: added matcard and matbutton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
+import { MatCardModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StringInterpolationComponent
-  ],
+  declarations: [AppComponent, StringInterpolationComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
